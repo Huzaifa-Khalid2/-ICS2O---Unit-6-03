39bb159fdd157f91be577ec74c9be2135bd24da1
HuzaifaKhalid2 <huzaifa.khalid@ocsbstudent.ca>
2020-12-18 15:59:48 +0000

    fixed javascript standard

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-document.getElementById("myBtn").addEventListener("click", myFunction);
-function myFunction() {
-let age = document.getElementById("my text").value;
-age=parseInt(age);
-if(age >= 17) {
-  alert("You can see a R rated movie.");
-} else if(age >= 17) {
-  alert("You can see a PG-13 move movie.");
-} else if(age >= 13) {
-  alert("You can see a G or PG move movie.");
+document.getElementById('my-btn').addEventListener('click', myFunction)
+function myFunction () {
+  let age = document.getElementById('my-text').value
+  age = parseInt(age)
+  if (age >= 17) {
+    alert('You can see a R rated movie.')
+} else if (age >= 17) {
+    alert('You can see a PG-13 move movie.')
+} else if (age >= 13) {
+    alert('You can see a G or PG move movie.')
 } else {
-   alert("ummmmmm you are too young for most things.");
+    alert('ummmmmm you are too young for most things.')
 }
-alert("Thank you for verifying your age!");
+  alert('Thank you for verifying your age!')
 }
